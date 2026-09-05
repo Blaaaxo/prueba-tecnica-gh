80b8f154356fdf7df21a6e7622d4e72c4d6319ad
Blaaaxo <blaaaa.xo0@gmail.com>
2026-09-05 18:42:34 -0500

    feat(frontend): configurar Tailwind CSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section >
+        <div className="text-red-600">
+          Rojo
+        </div>
+        
+      </section>
+    </>
+  )
+}
+
+export default App
